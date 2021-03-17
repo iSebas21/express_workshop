76ff2a7e5a9fbb082d3a3dbd9b638a5206edf615
Sebastian Manzo <sebas_092100@outlook.com>
2021-03-16 18:03:36 -0600

    Pokedex
    Implementacion de pokedex

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express')
 const app = express()
+const {pokemon} = require('./pokedex.json')
 
 app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Hola mundo")
+    res.status(200)
+    res.send("Bienvenido a la Pokedex")
 })
 
-app.listen(3000, () => {
+app.get("/pokemon/all", (req, res, next) => {
+    res.status(200)
+    res.send(pokemon)
+})
+
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
+    const id = req.params.id -1
+    if(id >= 0 && id <= 150) {
+        res.status(200)
+        res.send(pokemon[req.params.id - 1])
+    }
+    else {
+        res.status(404)
+        res.send("Pokemon no encontrado")
+    }
+})
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const name = req.params.name
+    for(i = 0; i < pokemon.length; i++) {
+        if(pokemon[i].name == name) {
+            res.status(200)
+            res.send(pokemon[i])
+        }
+    }
+    res.status(404)
+    res.send("Pokemon no encontrado")
+})
+
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...")
 })
