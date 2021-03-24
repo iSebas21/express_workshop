eaf71b808897c1150df19c8d06e99fe6567b9def
Sebastian Manzo <sebas_092100@outlook.com>
2021-03-24 13:17:36 -0600

    Implementacion de base de datos relacional y Morgan

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,38 +1,36 @@
-const express = require('express')
-const pokemon = express.Router()
-const db = require('../config/database.js')
+const express = require('express');
+const pokemon = express.Router();
+const db = require('../config/database');
 
-pokemon.post("//", (req, res, next) => {
-    return res.status(200).send(req.body.name)
+pokemon.post("/",(req, res ,next) =>{
+    return res.status(200).send(req.body);
+});
+
+pokemon.get("/",async(req, res, next) => {
+    const pkmn = await db.query("SELECT * FROM pokemon");
+    return res.status(200).json(pkmn);
 })
 
-pokemon.get("/", async (req, res, next) => {
-    const pkmn = await db.query("SELECT * FROM pokemon" )
-    return res.status(200).json(pkmn)
-})
-
-pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
-    const id = req.params.id
-    if (id >= 0 && id <= 722) {
-        const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = " + id )
-        return res.status(200).json(pkmn)
-    }else{
-        res.status(404).send("Pokemon no encontrado")
+pokemon.get('/:id([0-9]{1,3})',async(req, res, next) =>{
+    const id = req.params.id -1;
+    const pkmn = await db.query("SELECT * FROM pokemon");
+    if (id >= 0 && id <= 721) {
+        return res.status(200).json(pkmn[req.params.id -1]);
     }
-})
+    return  res.status(404).send("Pokemon no encontrado");
+});
 
-pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
-    const name = req.params.name
-    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name = " + name )
-    const pk = pkmn.filter((p) => {
-        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null    
-    })
+pokemon.get('/:name([A-Za-z]+)', async(req, res ,next) => {
+    const name = req.params.name;
+    const pkmn = await db.query("SELECT * FROM pokemon");
+    const pkmon = pkmn.filter((p) => {
+       return (p.pok_name.toUpperCase() == name.toUpperCase()) && p;
+    });
 
-    if (pk.length > 0) {
-        return res.status(200).json(pkmn) 
-    }else {
-        return res.status(404).send("Pokemon no encontrado")
+    if(pkmon.length>0) {
+        return res.status(200).json(pkmon)
     }
-})
+    return res.status(404).send("Pokemon no encontrado");
+});
 
-module.exports = pokemon
+module.exports = pokemon;
