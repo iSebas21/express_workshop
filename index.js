54f7639475db93d9d287cf6123d714e33b486d08
Sebastian Manzo <sebas_092100@outlook.com>
2021-03-23 22:14:29 -0600

    Implementacion de base de datos relaciona y la herramienta Morgan

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const express = require('express')
 const app = express()
-const {pokemon} = require('./pokedex.json')
+const pokemon = require('./routes/pokemon.js')
 
+app.use(morgan('dev'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res, next) => {
     return res.status(200).send("Bienvenido a la Pokedex")
 })
 
-app.post("/pokemon/", (req, res, next) => {
-    return res.status(200).send(req.body.name)
-})
-
-app.get("/pokemon", (req, res, next) => {
-    return res.status(200).send(pokemon)
-})
-
-app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
-    const id = req.params.id -1
-    return (id >= 0 && id <= 150) ? 
-        res.status(200).send(pokemon[req.params.id - 1]) :
-        res.status(404).send("Pokemon no encontrado")
-})
-
-app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
-    const name = req.params.name
-
-    const pk = pokemon.filter((p) => {
-        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null    
-    })
-
-    return (pk.length > 0) ? 
-        res.status(200).send(pk) : 
-        res.status(404).send("Pokemon no encontrado")
-})
-
+app.use("/pokemon", pokemon)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...")
