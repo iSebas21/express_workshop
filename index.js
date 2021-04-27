eae459e122bdb113df81937cbe12d34e3620494f
Sebastian Manzo <sebas_092100@outlook.com>
2021-04-27 17:24:23 -0500

    Implementacion de JSW

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
+//Dependencies
 const morgan = require('morgan');
 const express = require('express');
+//Routers
 const app = express();
-const pokemon = require('./routes/pokemon.js');
-const user = require('./routes/user.js')
+const pokemon = require('./routes/pokemon');
+const user = require('./routes/user')
+//Middleware
+const auth = require('./middleware/auth')
+const notFound = require('./middleware/notFound')
+const index = require('./middleware/index')
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res, next) => {
-    return res.status(200).json({code:1, message: "Bienvenido a la Pokedex"});
-});
-
+app.get("/", index);
+app.use("/user", user);
+app.use(auth);
 app.use("/pokemon", pokemon);
-app.use("/user", user)
-
-app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "URL inexistente"});
-});
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
