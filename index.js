e64ff7604c0739b476f4d9bd32ddace76953cf97
iSebas21 <sebas_092100@outlook.com>
2021-04-29 22:29:22 -0500

    Conexion

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 //Dependencies
 const morgan = require('morgan');
 const express = require('express');
-//Routers
 const app = express();
+//Routers
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user')
 //Middleware
-const auth = require('./middleware/auth')
-const notFound = require('./middleware/notFound')
-const index = require('./middleware/index')
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
+const cors = require('./middleware/cors');
 
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", index);
-app.use("/user", user);
+app.get('/', index);
+app.use('/user',user);
 app.use(auth);
-app.use("/pokemon", pokemon);
+app.use('/pokemon', pokemon);
 app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running...");
+    console.log('Server is runing...');
 });
--- routes/user.js
+++ routes/user.js
@@ -1,52 +1,46 @@
-const express = require('express');
+const expres = require('express');
 const jwt = require('jsonwebtoken');
-const user = express.Router();
+const user = expres.Router();
 const db = require('../config/database');
 
-user.post("/signin", async(req, res, next) => {
-    const { user_name, user_mail, user_password } = req.body;
-
-    if (user_name && user_mail && user_password) {
-        let query = "INSERT INTO user (user_name, user_mail, user_password) ";
-        query += `VALUES ('${user_name}', '${user_mail}', '${user_password}');`;
+user.post("/signin", async (req, res, next) => {
+    const {user_name, user_mail, user_password} = req.body
+    if(user_name && user_mail && user_password) {
+        let query = "INSERT INTO user (user_name, user_mail, user_password)"; 
+        query += ` VALUES ('${user_name}', '${user_mail}', '${user_password}');`;
         const rows = await db.query(query);
-
         if(rows.affectedRows == 1) {
-            return res.status(201).json({ code:201, message: "Usuario registrado" });
+            return res.status(201).json({code:201, message: "Usuario registrado correctamente"});
         }
-        return res.status(500).json({ code: 500, message: "Ocurrio un error" });
+        return res.status(500).json({code:500, message: "Ocurrio un error"});
     }
-    return res.status(500).json({ code: 500, message: "Campos incompletos" });
+    return res.status(500).json({code:500, message: "Campos incompletos"});
 });
 
-user.post("/login", async(req, res, next) =>{
-    const { user_mail, user_password }= req.body;
-    const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`;
+user.post('/login', async (req, res, next) => {
+    const {user_mail,user_password} = req.body;
+    let query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND `;
+    query += `user_password = '${user_password}';`;
     const rows = await db.query(query);
-
-    if (user_mail && user_password) {
-        if (rows.length == 1) {
+    if(user_mail && user_password){
+        if(rows.length == 1) {
             const token = jwt.sign({
                 user_id: rows[0].user_id,
                 user_mail: rows[0].user_mail
             }, "debugkey");
-            return res.status(200).json({code: 200, message:token});
-        }
-        else {
-            console.log(rows)
-            return res.status(401).json({code: 401, message: "Usuario y/o contraseña incorrectos"});
+            return res.status(200).json({code: 200, message: token});
+        } else
+        {
+            return res.status(200).json({code: 200, message: "Usuario y/o contraseña incorrectos"});
         }
     }
-    return res.status(500).json({code: 500, message: "Campos incompletos"});
-    
+    return res.status(500).json({code:500, message: "Campos incompletos"});
 });
 
-user.get("/", async (req, res, next) => {
+user.get('/', async (req, res, next) => {
     const query = "SELECT * FROM user";
     const rows = await db.query(query);
-
-    return res.status(200).json({ code: 200, message: rows })
-
+    return res.status(200).json({ code: 200, message: rows });
 });
 
 module.exports = user;
